dc3884132a9fa7f3e75dbd7fe4fc53e7e4ddac95
Alex <kalfas.alex@gmail.com>
2022-03-26 22:20:36 +0200

    Add premature benchmarking template

---
+++ AdventOfCSharp.Template.Benchmarks/Program.cs
@@ -0,0 +1,7 @@
+﻿using AdventOfCSharp.Benchmarking;
+using AdventOfCode.Benchmarks;
+
+// Enter the base directory that will contain the problem files where
+// You can only have one problem input directory
+const string problemFileBaseDirectory = @"custom path";
+BenchmarkDescriberExecution.RunBenchmark<Consumer>(problemFileBaseDirectory);
